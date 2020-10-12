5f338a7549ddcd00bd0b81f754d6cd940bf65e1f
Nicolas Feroux <nicoa.cn@gmail.com>
2020-10-12 20:00:15 +0200

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: [
+		"@nuxtjs/eslint-config-typescript",
+		"prettier",
+		"prettier/vue",
+		"plugin:prettier/recommended",
+		"plugin:nuxt/recommended"
+	],
+	plugins: ["prettier"],
+	// add your custom rules here
+	rules: {
+		"@typescript-eslint/interface-name-prefix": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		indent: ["error", "tab"],
+		quotes: [
+			"error",
+			"double",
+			{
+				allowTemplateLiterals: true,
+				avoidEscape: true
+			}
+		],
+		semi: ["error", "never", { beforeStatementContinuationChars: "never" }],
+		"no-multi-spaces": "error",
+		"no-trailing-spaces": "error",
+		"arrow-spacing": "error"
+	}
+}
